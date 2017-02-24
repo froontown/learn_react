08fe2a14452a2a0a1ee1f05f1e03b29e8f187f91
froontown <froontown@froontowns-MacBook-Pro.local>
2017-02-24 14:33:54 -0500

    moving onto JSX for now

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from 'React-DOM';
-
 var HelloWorld = React.createClass({
   render: function () {
     return React.createElement('h1', null, "Hello World from Learning ReactJS");
